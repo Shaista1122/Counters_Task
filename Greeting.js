9c6efdd80ee263b24f8b9efed097ad97410ae9b4
Shaista1122 <100231970+Shaista1122@users.noreply.github.com>
2022-04-05 01:00:57 -0700

    Add files via upload

---
+++ Greeting.js
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+import { Text, TextInput, View } from 'react-native';
+const Greeting= () => {
+    const [name, setName] = useState("Sarah");
+    
+    return (
+        <View>
+           <TextInput placeholder="Enter your name..."
+                      onChangeText={text => setName(text)}
+            />
+           <Text>Hello {name}!</Text>
+        </View>
+   );
+}
+export default Greeting;
